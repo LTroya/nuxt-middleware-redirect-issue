5691b3d6c52d9d5b09bce36e0869cbc6a204c52d
ltroya-as <luis.troya@allansolutions.com>
2021-09-29 19:35:11 -0500

    Add example for middleware

---
+++ pages/dummy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/">Dummy</nuxt-link>
+    <div>Dummy page</div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware({ route, redirect }) {
+    console.log(process.static);
+    if (process.static) {
+      return;
+    }
+
+    if (route.path.includes("dummy")) {
+      return redirect("/");
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/">Dummy</nuxt-link>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
